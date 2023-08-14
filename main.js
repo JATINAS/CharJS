f0808c9e41939cdd85f835bdb398ea2c0ea7c086
JATINAS <jairpc385@gmail.com>
2023-08-13 23:06:00 -0700

    ya despliga datos

--- main.js
+++ main.js
@@ -6,44 +6,6 @@ function Datos () {
             const etiquetas = resultado.data.data.map(item => {
                 return item.name.toUpperCase();
             })
-
-            //valores a comparar
-            const precio = resultado.data.data.map(item => {
-                return parseInt(item.priceUsd/1000)
-            })
-/*
-            const myChart = new Chart(grafico, {
-                type:"line",
-                data:{
-                    labels: etiquetas,
-                    datasets:[
-                        {
-                            label: "Valor del Dolar",
-                            data: precio,
-                            fill: true,
-                            backgroundColor: "blue",
-                            borderColor: "black"
-                        },
-                    ],
-                },
-                options: {
-                    responsive: true,
-                    plugins: {
-                        legend: {
-                            position: 'top',
-                        },
-                        title: {
-                            display: true,
-                            Text: "Monedas Electronicas"
-                        }
-                    },
-                    scales: {
-                        x: {
-                            display: true
-                        }
-                    }
-                }
-            })*/
             Valorestbl.innerHTML = "";
             for (const valor of resultado.data.data) {
                 if (parseInt(valor.priceUsd) > 100) {
