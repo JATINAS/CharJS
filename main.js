3c5d1c4183f6011565bab7ae40a015f8be7face3
JATINAS <jairpc385@gmail.com>
2023-08-13 19:13:04 -0700

    Se despliega grafica y tabla con valores del API

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+const tblValores = document.getElementById("tblValores") 
+
 function Datos () {
-    const grafico = document.getElementById("Chart").getContext("2d")
+    const grafico = document.getElementById("myChart").getContext("2d")
     axios.get("https://api.coincap.io/v2/assets").then(resultado => {
             const etiquetas = resultado.data.data.map(item => {
                 return item.name.toUpperCase();
@@ -10,8 +12,47 @@ function Datos () {
                 return parseInt(item.priceUsd)
             })
 
-            console.log(precio);
-            console.log(etiquetas);
+            const myChart = new Chart(grafico, {
+                type:"line",
+                data:{
+                    labels: etiquetas,
+                    datasets:[
+                        {
+                            label: "Valor del Dolar",
+                            data: precio,
+                            fill: true,
+                            backgroundColor: "blue",
+                            borderColor: "black"
+                        },
+                    ],
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'top',
+                        },
+                        title: {
+                            display: true,
+                            Text: "Monedas Electronicas"
+                        }
+                    },
+                    scales: {
+                        x: {
+                            display: true
+                        }
+                    }
+                }
+            })
+            Valorestbl.innerHTML = "";
+            for (const valor of resultado.data.data) {
+                let tr = `<tr>
+                <td>${valor.symbol}<td>
+                <td>${valor.name}<td>
+                <td>${parseInt(valor.priceUsd)}<td>
+                </tr>`
+                Valorestbl.innerHTML += tr;
+            }
     })
 }
 
